02f232a61eea85407c5c194ae4dc4bd56ccf81da
SaturnsMoon7 <eduardf@live.wsd1.org>
2024-04-15 22:39:12 -0500

    April 15 Night

---
+++ BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount 
+{
+    private int balance;
+
+    public BankAccount(int initialBalance)
+    { this.balance = initialBalance;}
+
+    public void deposit(int amt)
+    { balance ++; }
+
+    public void withdraw(int amt)
+    { 
+        if (amt > balance)
+        { System.out.println("Insufficient funds"); }
+
+        else
+        { balance --; }
+    }
+        
+    public int getBalance()
+    {
+        return balance;
+    }
+}
---
+++ SavingsAccount.java
@@ -0,0 +1,14 @@
+public class SavingsAccount extends BankAccount
+{
+    public SavingsAccount(int initialBalance)
+    { super(initialBalance); }
+
+    @Override
+    public void withdraw(int amt)
+    {
+        if (getBalance() - amt < 100)
+        { System.out.println("Balance above $100 must be maintained"); }
+        else
+        { super.withdraw(amt); }
+    }
+}
